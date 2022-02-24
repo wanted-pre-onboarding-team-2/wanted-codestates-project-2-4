94613c77037a4e4b53a3442f937bb6a8338c272c
Parksangwoo <psu8430@gmail.com>
2022-02-24 23:24:44 +0900

    Feat: Card 컴포넌트 제작

---
+++ src/components/Card/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  width: 320px;
+  height: 180%;
+`;
+
+export const CardThumbnail = styled.img`
+  width: 100%;
+`;
+
+export const CardInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CardButton = styled.button``;
