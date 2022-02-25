2a08aa4ab095e7d1c391158948e06c2c7c369fdd
hoyong <wjsghdyd4538@yonsei.ac.kr>
2022-02-25 10:57:08 +0900

    feat : Tab redux state 관리 로직 구현

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import contents from "./contents";
+import tab from "./tab";
 
 const rootReducer = combineReducers({
   contents,
+  tab,
 });
 
 export default rootReducer;
