9e8c461ed1274804ca2ee37429be9599cc434951
min <hororonoodle@gmail.com>
2022-02-25 17:18:18 +0900

    feat : store 코드 변경

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./App";
@@ -9,10 +9,12 @@ import GlobalStyle from "./GlobalStyle";
 import rootReducer from "./modules";
 import ReduxThunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(ReduxThunk)),
-);
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(ReduxThunk))
+    : composeWithDevTools(applyMiddleware(ReduxThunk));
+
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
