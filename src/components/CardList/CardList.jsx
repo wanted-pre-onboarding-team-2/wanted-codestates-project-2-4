549182158256b1bdd10a7fefe7bdbca2056befc7
Kae <kaehehehe@gmail.com>
2022-02-26 14:52:24 +0900

    Remove: 불필요한 코드 제거

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -16,13 +16,11 @@ function CardList() {
   useEffect(() => {
     if (contents.data) {
       const index = tab - 1;
-      if (contents.data.sector) {
-        setSector(contents.data.sector[index]);
-        setType(contents.data.sector[index].type);
-        const data = contents.data.content;
-        const result = data.filter(item => item.sector_id === tab);
-        setContent(result);
-      }
+      setSector(contents.data.sector[index]);
+      setType(contents.data.sector[index].type);
+      const data = contents.data.content;
+      const result = data.filter(item => item.sector_id === tab);
+      setContent(result);
     }
   }, [contents, tab]);
 
